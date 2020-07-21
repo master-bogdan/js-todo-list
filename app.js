27b6f94540618bfa2928def9e679102f4eb00518
masterbogdan <maiskiychavk@gmail.com>
2020-07-21 23:03:23 +0300

    add new function *localStorage render *render for items *start make delete task function

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
+window.addEventListener('DOMContentLoaded',() => {
+
 const form = document.querySelector('form');
 const input = document.querySelector('#add-task');
 const out = document.querySelector('.list-group');
+const list = document.querySelector('.list-group');
 
-const test = (event) => {
-    event.preventDefault();
-    console.log('test');
-    console.log(input.value);
-    // localStorage.setItem(input.value, 'test');
-    out.insertAdjacentHTML('afterbegin', `<li class="list-group-item">${input.value}</li>`);
+// Функция отрисовки зданий из локального хранилища
+// Local satorage render function
+
+const renderStorage = () => {
+    let task = Object.entries(localStorage);
+    task.map(([key, value]) => renderTask(key, value));
 };
 
-form.addEventListener('submit', test);
+renderStorage();
 
-function render() {
+// Функция добваления задачи
+// Function for adding tasks
+const addTask = (event) => {
+    let count = localStorage.length;
+    event.preventDefault();
+    localStorage.setItem( count + 1 , input.value);
+    renderTask(count + 1, input.value);
+};
 
+form.addEventListener('submit', addTask);
+
+// Функция рендер задачи
+// Task render function
+function renderTask(id, value) {
+    out.insertAdjacentHTML('beforeend', 
+    `<li id=${id} class="list-group-item">
+        <div class="d-flex justify-content-between important">
+            <span><input type="checkbox"> ${value}</span>
+            <div class="d-flex justify-content-center align-items-center">
+                <button type="button" class="btn-star btn-sm"><i class="fas fa-star"></i></button>
+                <button type="button" class="btn-trash btn-sm"><i class="fas fa-trash-alt"></i></button>
+            </div>
+        </div> 
+    </li>`
+    );
 }
+
+const deleteBtn = document.querySelector('.btn-trash');
+const deleteTask = () => {
+
+};
+// deleteBtn.addEventListener('click')
+
+});
