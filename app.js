6e1552ad2a088efbbf29250b9fd0d94051ee5bc6
masterbogdan <maiskiychavk@gmail.com>
2020-07-24 09:00:49 +0300

    add checkedTask function

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ let todoListItems = [];
 
 // Функция рендер задачи
 // Task render function
-const renderTask = (id, value, checked) => {
+const renderTask = ({id, value, checked}) => {
     list.insertAdjacentHTML('beforeend', 
     `<li id=${id} class="list-group-item">
         <div class="d-flex justify-content-between important">
-            <span><input type="checkbox"> ${value} </span>
+            <div class="d-flex justify-content-between align-items-center">
+            <input type="checkbox"><span>${value}</span>
+            </div>
             <div class="d-flex justify-content-center align-items-center">
                 <button type="button" class="btn-star btn-sm"><i class="fas fa-star"></i></button>
                 <button type="button" class="btn-trash btn-sm"><i class="fas fa-trash-alt"></i></button>
@@ -28,7 +30,7 @@ const renderStorage = () => {
     if (localStorage.getItem('todo') != undefined) {
         todoListItems = JSON.parse(localStorage.getItem('todo'));
     }
-    todoListItems.map((item) => renderTask(item.id, item.value, item.checked, item.date));
+    todoListItems.map((item) => renderTask(item));
 };
     
 renderStorage();
@@ -43,24 +45,19 @@ const addTask = (event) => {
             value: input.value, 
             checked: false,
         };
-        todoListItems.push(todoListItem);
+    todoListItems.push(todoListItem);
     input.value = '';
     localStorage.setItem('todo', JSON.stringify(todoListItems));
-    renderTask(todoListItem.id, todoListItem.value, todoListItem.checked);
+    renderTask(todoListItem);
 };
 
 form.addEventListener('submit', addTask);
 
-
+// Функция удаления задачи
+// Delete task function
 const deleteTask = (event) => {
     let target = event.target;
     if (target.classList.contains('btn-trash') || target.classList.contains('fa-trash-alt')) {
-        // todoList.map((item, index) => {
-        //     if (item.id == target.closest('.list-group-item').id) {
-        //         console.log(item.id);
-        //        todoList.splice(item[index], 1).sort();
-        //     }
-        // });
         const index = todoListItems.findIndex(item => item.id == target.closest('.list-group-item').id);
         if (index !== -1) {
             todoListItems.splice(index, 1);
@@ -71,6 +68,18 @@ const deleteTask = (event) => {
 };
 
 list.addEventListener('click', deleteTask);
+
+const checkedTask = (event) => {
+    let target = event.target.checked;
+    if (target) {
+        event.target.nextSibling.classList.add('strike');
+    }
+    else {
+        event.target.nextSibling.classList.remove('strike');
+    }
+};
+
+list.addEventListener('change', checkedTask);
 
 console.log(todoListItems);
 });
